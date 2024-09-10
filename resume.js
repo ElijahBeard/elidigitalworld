eb965d0b0b4e2f3902aa8483b195be1bf2c321aa
“ElijahBeard” <elijahbeard24@gmail.com>
2024-09-10 16:34:28 -0400

    started portfolio, added js typing function

--- resume.js
+++ resume.js
@@ -1,4 +1,5 @@
 grabbed = false;
+window.onbeforeunload = window.scrollTo(0, 0);
 
 function mousemove(event){
     let tab = document.getElementById("pull_tab");
@@ -20,7 +21,7 @@ function is_in_bounding_box(x,y){
     tab_top = tab.getBoundingClientRect().top.toFixed(0);
     tab_bottom = tab.getBoundingClientRect().bottom.toFixed(0);
     scroll_position = y/document.body.scrollHeight * 100;
-    console.log(scroll_position);
+
     if(y > 100 && scroll_position < 88 && x_p > 40 && x_p < 60 && (y > tab_top - 25 && y < tab_bottom + 10) ){
         return true;
     }
@@ -30,6 +31,42 @@ function grab(event){
     if(event.type == 'mousedown'){
          grabbed = true;
     } else {grabbed = false;}
+}
+
+document.getElementById("name").onload = type(0);
+function type(i){
+
+    var txt = ' Elijah Beard';
+    var phone = ' 614-896-0265';
+    var addy = ' Athens, Ohio';
+    var email = ' elijahbeard24@gmail.com';
+
+    let doc_name = document.getElementById("name");
+    let doc_location = document.getElementById("location");
+    let doc_phone = document.getElementById("phone");
+    let doc_email = document.getElementById("email");
+    if(i < left_main.childElementCount){
+       document.getElementById("left_main").children[i].style = "opacity:1;";
+        document.getElementById("right_main").children[i].style = "opacity:1;";
+    }
+    
+    if(i <= txt.length){
+        doc_name.innerHTML += txt.charAt(i);
+    } 
+    if(i <= phone.length){
+        doc_phone.innerHTML += phone.charAt(i);
+    } 
+    if(i <= addy.length){
+        doc_location.innerHTML += addy.charAt(i);
+    } 
+    if(i <= email.length){
+        doc_email.innerHTML += email.charAt(i);
+    } else {return}
+
+
+    setTimeout(() => {
+        type(i+1);
+    }, 90);
 }
 
 document.addEventListener('mousemove',mousemove);
