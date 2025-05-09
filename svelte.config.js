33d969c12baed4653d269b5a487653773e60248b
ElijahBeard <elijahbeard24@gmail.com>
2025-05-09 00:34:38 -0400

    added adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node'; // or adapter-auto or adapter-vercel
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
