fae26a1b3e2664eea56d0a36f3fedd26bde6fec4
ElijahBeard <elijahbeard24@gmail.com>
2026-05-07 13:23:19 -0400

    updated portfolio

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+	import { page } from '$app/state';
+
+    import Projectcard from './projectcard.svelte';
+    import Windfield from '$lib/components/windfield.svelte';
+
+    let windfield!:Windfield
+    
+    // Reactive resizing
+    let width = 0
+    let mobile
+    $: mobile = width <= 600
+    onMount(() => {
+        width = window.innerWidth;
+    });
+
+    let scrollY = 0;
+
+    function handleScroll(e: Event) {
+        scrollY = (e.target as HTMLElement).scrollTop;
+        windfield?.onScroll(scrollY);
+    }
+
+    import { projects } from './projects'
+    
+</script>
+
+<svelte:window bind:innerWidth={width} />
+<div id="windfield"><Windfield bind:this={windfield}/></div>
+<main
+    on:scroll={handleScroll}
+    style="--accent:{page.data.accent}; --bg:{page.data.bg};"
+>
+    <h1 id="title" style="font-family:block-logo;{page.data.accent};">{mobile ? "PRTF0LIO" : "PORTF0LIO"}</h1>
+    <div class="welcome" style="width:70vw;">
+        <!-- <button on:click={() => {windfield.move();}}>PROJECTS</button> -->
+    </div>
+    {#each projects as project}
+        <Projectcard
+            title={project.title}
+            link={project.link}
+            iframe={project.iframe}
+            image={project.image}
+            description={project.description}
+        />
+    {/each}
+</main>
+<style>
+    main {
+        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        position:absolute;
+        top:0;
+        z-index: 4;
+        display:flex;
+        flex-direction: column;
+        width:100vw;
+        background-color: var(--bg);
+        height:100vh;
+        padding:12px;
+        overflow-y:scroll;
+        overflow-x:hidden;
+    }
+    #title {
+        align-self: center;
+        margin:0;
+        padding:0;
+        font-size:48pt;
+        overflow-wrap: break-word;
+        color: var(--accent);
+        letter-spacing: 0.05em;
+        word-spacing: -29px;
+    }
+    p {
+        margin:10px;
+        font-size:23px;
+        text-wrap: break-word;
+    }
+    .stack {
+        margin-bottom:10px;
+        z-index: 99;
+        display: flex;
+        flex-wrap:wrap;
+        gap: 8px;
+        margin-left:3px;
+        mix-blend-mode: difference;
+    }
+    #paint {
+        margin-left:6px;
+        z-index: 999;
+    }
+    #windfield{
+        position:absolute;
+        width:100vw;
+        height:100vh;
+        top:0;
+        z-index: 0;
+        pointer-events:none;
+        mix-blend-mode:difference;
+    }
+    button {
+        background-color: gray;
+        border-style:dotted;
+        margin-left:6px;
+    }
+</style>
---
+++ src/routes/portfolio/projects.ts
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        title: "Acrn Live",
+        link: "https://acrnlive.web.app",
+        iframe: "https://acrnlive.web.app",
+        description: "Acrn live is a radio streaming system built for ACRN, Ohio University's rock radio station. Built with TypeScript, SvelteKit, Firebase, and MongoDB and supports over 1,200 monthly users."
+    },
+    {
+        title: "Ceramic Synth",
+        image: "/portfolio/ceramic.png",
+        link: "https://github.com/ElijahBeard/ceramic-synth",
+        description: "Multidisciplinary audio hardware project built by Julia Weber and Eli Beard. An experimental audio generation circuit built on ceramic slabs."
+    },
+    {
+        title: "Sunshine & Rainbows: Autonomous Driving",
+        image: "/portfolio/turtlecar.jpg",
+        link: "#",
+        description: "A deep reinforcement learning approach to autonomous driving. A computer science capstone project advised by Dr. Yadav. Awarded 1st place at the Ohio University 2026 Student Expo."
+    },
+    {
+        title: "Synthesizer Sequencer",
+        image: "/portfolio/synthsequencer.jpeg",
+        link: "https://github.com/ElijahBeard/synth-sequencer",
+        description: "Blossomhack 2024 Best Hardware. Arduino based step sequencer interfaced with Mozzi synthesizer engine."
+    },
+    {
+        title: "GAM Data Analysis",
+        image: "/portfolio/community.png",
+        link: "https://github.com/ElijahBeard/data-mining-experiments",
+        description: "Part of coursework for CS 4150 Data Science Algorithms. A report on genomic feature tables and loci via analysis tools and visualizations."
+    },
+    {
+        title: "User-177606669",
+        image: "/portfolio/user.png",
+        link: "https://soundcloud.com/user-177606668",
+        description: "An international online music collective started in 2020. Artists from around the world upload to the page anonymously. The project has aquired 400+ members and released 5k+ tracks."
+    }
+];
